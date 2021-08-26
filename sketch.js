64b7f98d7d80f0171358442027894d8ef73e749d
kritika127 <88191594+kritika127@users.noreply.github.com>
2021-08-26 14:45:15 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,166 +1,98 @@
-var PLAY = 1;
-var END = 0;
-var gameState = PLAY;
+var canvas,bg;
+var together;
+var cat, catImg1,catImg2;
+var mouse, mouseImg1,mouseImg2;
 
-var ground,ground_image,invisible_ground;
-var girl,girl_running,girl_collided,girlImage,zombie,zombie_running,zombie_attack;
-var obstaclesGroup,obstacle1,obstacle2,obstacle3,obstacle4;
-var jumpSound,dieSound,checkpointSound;
-var score;
-var gameOver,restart,gameOverImage,restartImage;
-
-function preload(){
-ground_image=loadImage("Background.png");
-  girl_running=loadAnimation("Run (1).png","Run (2).png","Run (3).png","Run (4).png","Run (5).png","Run (6).png","Run (7).png","Run (8).png","Run (9).png","Run (10).png","Run (11).png","Run (12).png","Run (14).png","Run (15).png","Run (16).png","Run (17).png","Run (18).png","Run (19).png","Run (20).png");
-  zombie_running=loadAnimation("Walk (1).png","Walk (2).png","Walk (3).png","Walk (4).png","Walk (5).png","Walk (6).png","Walk (7).png","Walk (8).png","Walk (9).png","Walk (10).png");
-  zombie_attack=loadAnimation("Attack (2).png","Attack (3).png","Attack (4).png","Attack (5).png","Attack (6).png","Attack (7).png","Attack (8).png");
-  obstacle1=loadImage("obstacle1.png");
-  zombie_idle=loadImage("Stand.png");
-  jumpSound = loadSound("jump.mp3")
-  dieSound = loadSound("die.mp3")
-  checkPointSound = loadSound("checkPoint.mp3")
-  gameOverImage=loadImage("gameOver1.png");
-  restartImage=loadImage("restart1.png");
-  girl_collided=loadImage("Dead (30).png");
-  girlImage=loadImage("Idle (1).png");
+function preload() {
+    bg = loadImage("images/garden.png");
+    catImg1= loadAnimation("images/catSitting.png");
+    catImg2=loadAnimation("images/catWalking1.png","images/catWalking2.png");
+    catImg3= loadAnimation("images/catHappy.png");
+    mouseImg1=loadAnimation("images/mouseGift.png");
+    mouseImg2= loadAnimation("images/mouseTease1.png","images/mouseTease2.png");
+    mouseImg3=loadAnimation("images/mouseHappy.png");
 }
 
-function setup() {
- createCanvas(600,500);
-  
-ground=createSprite(0,0,0,0);
-  ground.shapeColor="white";
-ground.addImage("ground_image",ground_image);
-ground.scale=1.4;
-   ground.velocityX=-1
-  
-   girl=createSprite(300,420,600,10);
-  girl.addAnimation("girl_running",girl_running);
-  girl.addImage("girl_collided",girl_collided);
-  girl.addImage("girlImage",girlImage);
-  girl.scale=0.2;
- // girl.velocityX=2;
-  girl.debug=false;
-  girl.setCollider("rectangle",0,0,girl.width,girl.height)
-  
-  
-  zombie=createSprite(50,410,600,10);
-  zombie.addAnimation("zombie_running",zombie_running);
-  zombie.addAnimation("zombie_attack",zombie_attack);
-  zombie.addImage("zombie_idle",zombie_idle);
-  zombie.scale=0.2;
-  zombie.debug=false;
- // zombie.velocityY=-13;
- // zombie.velocityX=Math.round(random(1,2));
-  
-  invisible_ground=createSprite(300,470,600,10);
-  invisible_ground.visible=false;
-  
-   gameOver = createSprite(300,100);
-  gameOver.addImage(gameOverImage);
-  
-  restart = createSprite(300,180);
-  restart.addImage(restartImage);
-  
-  obstaclesGroup=new Group();
-  
-  score=0;
+function setup(){
+    canvas = createCanvas(1000,800);
+
+    mouse = createSprite(200, 600);
+    mouse.addAnimation("mouseStanding", mouseImg1);
+    mouse.scale = 0.15;
+    
+    cat = createSprite(900, 600);
+    cat.addAnimation("catSleeping", catImg1);
+    cat.scale = 0.25;
+
+    makeCatWalk();
+
 }
 
 function draw() {
- background("black");
-  
- // console.log(girl.y);
-   //Gravity
-girl.velocityY = girl.velocityY + 0.8;
-girl.collide(invisible_ground); 
-  
-   //Gravity
-zombie.velocityY = zombie.velocityY + 0.8;
-zombie.collide(invisible_ground); 
-  
-  
-   if (gameState===PLAY){
-    gameOver.visible=false;
-  restart.visible=false;
-    //  zombie.y=girl.y;
-   score = score + Math.round(getFrameRate()/60);
- 
-    spawnObstacles();
-   if (obstaclesGroup.isTouching(zombie)){
-     zombie.velocityY=-12;
-   }
- ground.velocityX = -(4 + 3* score/100);
-     
-   if (ground.x < 0){
-      ground.x = ground.width/2;
+
+    background(bg);
+
+//CHOOSE THE CORRECT IF BLOCK FOR CHECKING COLLISION 
+
+    /*if(cat.y - mouse.x < (cat.height - mouse.width)/2)
+    { 
+        cat.velocityX=0;
+        cat.addAnimation("catLastImage", catImg3);
+        cat.x =300;
+        cat.scale=0.2;
+        cat.changeAnimation("catLastImage");
+        mouse.addAnimation("mouseLastImage", mouseImg3);
+        mouse.scale=0.15;
+        mouse.changeAnimation("mouseLastImage");
+    }*/
+
+    if(cat.x - mouse.y < (cat.width - mouse.y)/2)
+    { 
+        cat.velocityX= -4;
+        cat.addAnimation("catLastImage", catImg3);
+        cat.x =300;
+        cat.scale=0.2;
+        cat.changeAnimation("catLastImage");
+        mouse.addAnimation("mouseLastImage", mouseImg3);
+        mouse.scale=0.15;
+        mouse.changeAnimation("mouseLastImage");
     }
-  
-     if(score>0 && score%100 === 0){
-       checkPointSound.play() 
-    }
-    
- if((keyDown("space")&& girl.y >= 220)) {
-   girl.velocityY = -12;
-    jumpSound.play();
-  }  
-  
-  if (girl.isTouching(obstaclesGroup)){
-    gameState=END;
-     dieSound.play();
-  }
-  }
-else if ( gameState===END) {
-  gameOver.visible=true;
-  restart.visible=true;
-  ground.velocityX = 0;
-     girl.velocityY = 0
-    girl.changeImage("girlImage",girlImage);
-  zombie.changeAnimation("zombie_attack",zombie_attack);
-     zombie.x=girl.x;
-  if (zombie.isTouching(girl)) {
-    girl.changeImage("girl_collided",girl_collided);
-    zombie.changeImage("zombie_idle",zombie_idle);
-  }
-    
-    obstaclesGroup.setLifetimeEach(-1);
-   obstaclesGroup.setVelocityXEach(0);
-  
-    if(mousePressedOver(restart)) {
-      reset();
-    }
-} 
-  
- 
-  drawSprites();
-  fill("lightpink");
-  textSize(20);
-   text("Score: "+ score, 500,50);
+
+ /*if(cat.x - mouse.x < (cat.width - mouse.width)/2)
+    { 
+        cat.velocityX=0;
+        cat.addAnimation("catLastImage", catImg3);
+        cat.x =300;
+        cat.scale=0.2;
+        cat.changeAnimation("catLastImage");
+        mouse.addAnimation("mouseLastImage", mouseImg3);
+        mouse.scale=0.15;
+        mouse.changeAnimation("mouseLastImage");
+    }*/
+
+    /*if(cat.y - mouse.y < (cat.height - mouse.height)/2)
+    { 
+        cat.velocityX= 4;
+        cat.addAnimation("catLastImage", catImg3);
+        cat.x =300;
+        cat.scale=0.2;
+        cat.changeAnimation("catLastImage");
+        mouse.addAnimation("mouseLastImage", mouseImg3);
+        mouse.scale=0.15;
+        mouse.changeAnimation("mouseLastImage");
+    }*/
+
+    drawSprites();
 }
 
-function reset(){
-  gameState=PLAY;
-gameOver.visible=false;
-restart.visible=false;
-girl.changeAnimation("girl_running",girl_running);
-  obstaclesGroup.destroyEach();
-  score=0;
-  zombie.x=50;
-}
 
-function spawnObstacles() {
-   if (frameCount % 60 === 0){
-   var obstacle = createSprite(600,450,10,40);
-   obstacle.velocityX = -6 ;//+ score/100);
-   
-    //generate random obstacles
-   var rand = Math.round(random(1,6));
-     obstacle.addImage(obstacle1);
-   obstacle.scale=0.1;
-      obstaclesGroup.add(obstacle);
-    obstacle.debug=false;
-obstacle.setCollider("circle",0,0,1);
-   }
-     
-}
+function makeCatWalk(){
 
+        mouse.addAnimation("mouseTeasing", mouseImg2)
+        mouse.changeAnimation("mouseTeasing");
+        mouse.frameDelay = 25; 
+
+        cat.velocityX = -5; 
+        cat.addAnimation("catRunning", catImg2);
+        cat.changeAnimation("catRunning");
+}
